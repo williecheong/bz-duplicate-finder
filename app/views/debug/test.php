03c22036906ab956fc7653e2b8b2a003e2092750
Willie <cheongwillie@gmail.com>
2015-02-27 17:02:37 -0500

    automated functionality testing ready

--- app/views/debug/test.php
+++ app/views/debug/test.php
@@ -11,6 +11,11 @@
         <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet">
         <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
         <link href="/assets/vendor/ng-toaster/toaster.css" rel="stylesheet">
+        <style>
+            .container {
+                width: 95%;
+            }
+        </style>
     </head>
     <body ng-app="myApp" ng-controller="myController">
         <toaster-container toaster-options="{'time-out': 5000}"></toaster-container>
@@ -20,35 +25,83 @@
                 <!-- Table Headings -->
                 <thead>
                     <tr>
-                        <th>Input Bug List</th>
-                        <th>Expected Clusters</th>
-                        <th>Actual Clusters Output</th>
-                        <th>Execution Details</th>
+                        <th style="width:10%;">Case #</th>
+                        <th style="width:15%;">Input Bug List</th>
+                        <th style="width:20%;">Expected Groups</th>
+                        <th style="width:30%;">Actual Groups Output</th>
+                        <th style="width:25%;">Execution Details</th>
                     </tr> 
                 </thead>
                 <tbody>
-                    <tr ng-repeat="testCase in testCases">
+                    <tr ng-repeat="(index,testCase) in testCases">
                         <td>
+                            <button ng-click="runTest(index, testCase.input)" ng-disabled="testCase.loading" class="btn btn-primary">
+                                <i class="fa fa-terminal fa-lg"></i> {{ index }}
+                            </button>
+                        </td>
+                        <td>
+                            <a href="{{testCase.input | bugLink}}" target="_blank"><i class="fa fa-external-link-square"></i></a>
                             <span ng-bind="testCase.input | arrayToCsv"></span>
                         </td>
                         <td>
-                            <div ng-repeat="group in testCase.expected">
+                            <div ng-repeat="(groupNum, group) in testCase.expectedGroups">
+                                <strong>Group {{groupNum}}: </strong><br>
+                                <a href="{{group | bugLink}}" target="_blank"><i class="fa fa-external-link-square"></i></a>
                                 <span ng-bind="group | arrayToCsv"></span>
-                                <hr style="margin:0px;">
+                                <hr style="margin-top:0px;margin-bottom:20px;">
                             </div>
                         </td>
                         <td>
-                            <div ng-if="!testCase.output" class="text-center">
-                                <button class="btn btn-primary">
-                                    <i class="fa fa-terminal"></i> Run test
-                                </button>
+                            <div ng-repeat="group in testCase.outputGroups">
+                                <strong>Bugs:</strong> 
+                                <a href="{{group.bugs | bugLink}}" target="_blank"><i class="fa fa-external-link-square"></i></a>
+                                <span ng-bind="group.bugs | arrayToCsv"></span><br>
+                                <strong>Keywords:</strong> 
+                                <span ng-bind="group.keywords | arrayToCsv"></span><br>
+                                <strong>Similarity:</strong> 
+                                <span ng-bind="group.similarity | number:6"></span>
+                                <hr style="margin-top:0px;margin-bottom:20px;">
                             </div>
-                            <div ng-repeat="group in testCase.output">
-                                <span ng-bind="group | arrayToCsv"></span>
-                                <a href="">
-                                    <i class="fa fa-desktop"></i>
-                                </a>
-                                <hr style="margin:0px;">
+                        </td>
+                        <td>
+                            <div ng-if="testCase.meta">
+                                <div style="margin-top:0px;margin-bottom:10px;">
+                                    <div>
+                                        <span>Status:</span>
+                                        <strong ng-class="{'text-success':testCase.meta.result.type=='success', 'text-danger':testCase.meta.result.type=='fail'}">
+                                            {{ testCase.meta.result.type | uppercase}}
+                                        </strong>
+                                    </div>
+                                    <em ng-if="testCase.meta.result.reason" ng-bind="testCase.meta.result.reason"></em>
+                                </div>
+                                <div style="margin-top:0px;margin-bottom:5px;">
+                                    <span>Number of Groups Returned:</span>
+                                    <strong>{{ testCase.outputGroups.length }}</strong>
+                                </div> 
+                                <div style="margin-top:0px;margin-bottom:5px;">
+                                    <span>Bugzilla Retrieval:</span>
+                                    <strong>{{ testCase.meta.runtime.bugzillaForBugs | number:4 }} seconds</strong>
+                                </div>         
+                                <div style="margin-top:0px;margin-bottom:5px;">
+                                    <span>NLProcessing:</span>
+                                    <strong>{{ testCase.meta.runtime.bugsToBagsOfWords | number:4 }} seconds</strong>
+                                </div>         
+                                <div style="margin-top:0px;margin-bottom:5px;">
+                                    <span>Cross Pairing:</span>
+                                    <strong>{{ testCase.meta.runtime.bagOfWordsToSimilarPairs | number:4 }} seconds</strong>
+                                </div>         
+                                <div style="margin-top:0px;margin-bottom:5px;">
+                                    <span>Bug Grouping:</span>
+                                    <strong>{{ testCase.meta.runtime.similarPairsToGroups | number:4 }} seconds</strong>
+                                </div>         
+                                <div style="margin-top:0px;margin-bottom:5px;">
+                                    <span>Post Processing:</span>
+                                    <strong>{{ testCase.meta.runtime.postProcessingForBugGroups | number:4 }} seconds</strong>
+                                </div>         
+                                <div style="margin-top:0px;margin-bottom:5px;">
+                                    <span>Total Duration:</span>
+                                    <strong>{{ testCase.meta.runtime.totalRuntime | number:4 }} seconds</strong>
+                                </div>
                             </div>
                         </td>
                     </tr>
@@ -58,9 +111,10 @@
 
         <!-- Angular / Bootstrap Core JavaScript
         <!-- Placed at the end of the document so the pages load faster -->
-        <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular.min.js"></script>
         <script src="//cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.11.0/ui-bootstrap-tpls.min.js"></script>
         <script src="//cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.20/angular-animate.min.js"></script>
+        <script src="//dropbox.com/s/5xks0xxfz1mmqux/test_cases.js?dl=1"></script>    
         <script src="/assets/vendor/ng-timeago/ng-timeago.js"></script>
         <script src="/assets/vendor/ng-toaster/toaster.js"></script>
         <script src="/assets/js/debug_test.js"></script>
--- public/assets/js/debug_test.js
+++ public/assets/js/debug_test.js
@@ -1,38 +1,50 @@
 var app = angular.module('myApp', ['ui.bootstrap', 'ngtimeago', 'toaster']);
 
-app.controller('myController', function( $scope, $sce, $http, $filter, $modal, toaster ) {
+app.controller('myController', function( $scope, $sce, $http, $filter, toaster ) {
     
-    $scope.testCases = [
-        {
-            input : [123123,324234],
-            expected : [
-                [123123,345346],
-                [123124,3453346],
-            ],
-            output : [
-                [1235325,345346],
-            ]
-        }
-    ];
-
-    $scope.loadEvent = function() {
+    $scope.testCases = testCases;
+    $scope.runTest = function(key, bugList) {
+        $scope.testCases[key].loading = true;
         $http({
             'method': 'GET',
-            'url': '/api/v1/event?id='
+            'url': '/duplicates?debug=1&bugs=' + bugList.join(',')
         }).success(function(data, status, headers, config) {
-            if (data.length > 0) {
-                $scope.event = data[0];
-                $scope.loadParticipants();
-            } else {
-                toaster.pop('error', 'Error: ' + status, "Event not found");
+            $scope.testCases[key].loading = false;
+            $scope.testCases[key].outputGroups = data.duplicates;
+            $scope.testCases[key].meta = {
+                similarityRequirement : data.similarityRequirement,
+                runtime : data.runtimeInSeconds,
+                result : $scope.validate(
+                    $scope.testCases[key].expectedGroups,
+                    data.duplicates
+                )
+            };
+
+            if (data.duplicates.length == 0) {
+                toaster.pop('info', 'Status: ' + status, "No duplicate groups found for test case " + key + ".");
             }
+
         }).error(function(data, status, headers, config) {
             toaster.pop('error', 'Error: ' + status, data.message);
+            $scope.testCases[key].loading = false;
         });
     }; 
 
+    $scope.validate = function(expected, actual) {
+        if (expected.length != actual.length) {
+            return {"type":"fail", "reason":"Wrong number of bug groups returned."};
+        }
+        return {"type":"success"};
+    };
+
 }).filter('arrayToCsv', function() {
   return function(input) {
-    return (typeof input == 'array') ? input.join(',') : input ;
+    if (typeof input != 'object') { return input; }
+    if (input.length == 0) { return "Empty"; }
+    return input.join(', ');
+  };
+}).filter('bugLink', function() {
+  return function(input) {
+    return "https://bugzilla.mozilla.org/rest/bug?include_fields=id,summary,component,product&bug_id=" + input.join(', ');
   };
 });
